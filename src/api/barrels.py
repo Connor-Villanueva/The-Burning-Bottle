fc182e1d74fc799a4f59e9d731ce1002e2bab958
Connor-Villanueva <ckvillan@calpoly.edu>
2024-09-28 12:29:25 -0700

    Version 1

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -23,6 +23,21 @@ class Barrel(BaseModel):
 def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
     """ """
     print(f"barrels delievered: {barrels_delivered} order_id: {order_id}")
+    
+    num_green_ml = 0
+    gold = 0
+    
+    
+    for barrel in barrels_delivered:
+        gold -= barrel.price * barrel.quantity
+        if (barrel.potion_type == [0, 1, 0, 0]):
+            num_green_ml += barrel.ml_per_barrel * barrel.quantity
+    
+    with db.engine.begin() as connection:
+        num_green_ml += connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory")).fetchone()[0]
+        gold += connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory")).fetchone()[0]
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_ml = " + str(num_green_ml)))
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = " + str(gold)))
 
     return "OK"
 
@@ -32,25 +47,23 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     """ """
     print(wholesale_catalog)
 
+    purchase_plan = []
+
     with db.engine.begin() as connection:
-        inventory = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory")).fetchall()
-    
-    num_green_potions = inventory[0][2]
-    gold = inventory[0][4]
+        num_green_potions = (connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).fetchone())[0]
+        gold = (connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory")).fetchone())[0]
+
     if (num_green_potions < 10):
         for barrel in wholesale_catalog:
-            if (barrel.sku == "SMALL_GREEN_BARREL" and barrel.price < gold):
-                return [
+            if (barrel.sku == "SMALL_GREEN_BARREL" and barrel.price <= gold):
+                purchase_plan.append(
                     {
                         "sku": barrel.sku,
-                        "quantity": 1,
+                        "quantity": 1
                     }
-                ]   
+                )
+
+    #For future purchases, keeping track of gold within locally is necessay to prevent an invalid purchase plan
     
-    return [
-        {
-            "sku": "SMALL_RED_BARREL",
-            "quantity": 1,
-        }
-    ]
+    return purchase_plan
 
--- src/api/bottler.py
+++ src/api/bottler.py
@@ -20,6 +20,20 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int
     """ """
     print(f"potions delievered: {potions_delivered} order_id: {order_id}")
 
+    num_green_potions = 0
+    num_green_ml = 0
+    for potion in potions_delivered:
+        if (potion.potion_type == [0, 100, 0, 0]):
+            num_green_potions += potion.quantity
+            num_green_ml -= 100 * potion.quantity
+
+    with db.engine.begin() as connection:
+        num_green_potions += connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).fetchone()[0]
+        num_green_ml += connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory")).fetchone()[0]
+        
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_potions = " + str(num_green_potions)))
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_ml = " + str(num_green_ml)))
+        
     return "OK"
 
 @router.post("/plan")
@@ -28,18 +42,31 @@ def get_bottle_plan():
     Go from barrel to bottle.
     """
 
+    bottle_plan = []
+
     # Each bottle has a quantity of what proportion of red, blue, and
     # green potion to add.
     # Expressed in integers from 1 to 100 that must sum up to 100.
 
-    # Initial logic: bottle all barrels into red potions.
+    # Initial logic: bottle all barrels into green potions.
 
-    return [
-            {
-                "potion_type": [100, 0, 0, 0],
-                "quantity": 5,
-            }
-        ]
+    with db.engine.begin() as connection:
+        num_green_ml = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory")).fetchone()[0]
+    
+    num_green_potions = 0
+    while (num_green_ml % 100 == 0 and num_green_ml > 0) :
+        num_green_potions += 1
+        num_green_ml -= 100
+    
+    bottle_plan.append(
+        {
+            "potion_type": [0, 100, 0, 0],
+            "quantity": num_green_potions
+        }
+    )
+
+
+    return bottle_plan
 
 if __name__ == "__main__":
     print(get_bottle_plan())
