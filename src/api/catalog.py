2fa36925447574e81f78fdfd17dec38488200f78
Connor-Villanueva <ckvillan@calpoly.edu>
2024-09-29 16:36:14 -0700

    Update Catalog

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -11,12 +11,21 @@ def get_catalog():
     Each unique item combination must have only a single price.
     """
 
-    return [
+    catalog = []
+
+    with db.engine.begin() as connection:
+        num_green_potions = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).fetchone()[0]
+    
+    if (num_green_potions > 0):
+        catalog.append(
             {
-                "sku": "RED_POTION_0",
-                "name": "red potion",
-                "quantity": 1,
-                "price": 50,
-                "potion_type": [100, 0, 0, 0],
+                "sku": "green_potion_0",
+                "name": "green_potion",
+                "quantity": num_green_potions,
+                "price": 25,
+                "potion_type": [0, 100, 0, 0]
             }
-        ]
+        )
+
+
+    return catalog
