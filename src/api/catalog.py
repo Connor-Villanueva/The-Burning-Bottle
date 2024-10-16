255e6e95bb821f5c00b9d4dafdc32296895601be
Connor-Villanueva <ckvillan@calpoly.edu>
2024-10-16 12:16:55 -0700

    Update catalog.py
    Properly choose random potions to fill catalog if prob. requirement is not met

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -19,43 +19,43 @@ def get_catalog():
         potions = connection.execute(sqlalchemy.text(
             '''
                 WITH
-                current_day AS (
-                    SELECT latest_day as day
-                    FROM time_info
-                ),
-                daily_total AS (
-                    SELECT day, sum(quantity) as daily_total
-                    FROM completed_orders co
-                    GROUP BY co.day
-                ),
-                top_relative_proportions AS (
-                    SELECT co.potion_sku, ROUND(SUM(co.quantity)::decimal / dt.daily_total, 2) AS relative_potion_probability
-                    FROM completed_orders co
-                    JOIN daily_total dt ON co.day = dt.day
-                    JOIN current_day cd ON co.day = cd.day
-                    GROUP BY co.potion_sku, co.day, dt.daily_total
-                    HAVING ROUND(SUM(co.quantity)::decimal / dt.daily_total, 2) > 0.25
-                    ORDER BY relative_potion_probability DESC
-                    LIMIT 6
-                ),
-                top_quantities AS (
-                    SELECT potion_sku, potion_quantity
-                    FROM potion_inventory
-                    GROUP BY potion_sku
-                    ORDER BY potion_quantity DESC
-                    LIMIT 6
-                )
-
-            SELECT DISTINCT p.potion_sku, p.potion_name, p.potion_quantity, p.potion_price, p.potion_type, priority
-            FROM (
-                SELECT potion_sku, 1 AS priority FROM top_relative_proportions
-                UNION
-                SELECT potion_sku, 2 AS priority FROM top_quantities
-            ) AS combined_results
-            JOIN potion_inventory p ON combined_results.potion_sku = p.potion_sku
-            WHERE p.potion_quantity > 0
-            ORDER BY priority, potion_sku
-            LIMIT 6
+                    current_day AS (
+                        SELECT latest_day as day, latest_hour as hour
+                        FROM time_info
+                    ),
+                    daily_total AS (
+                        SELECT co.day, sum(co.quantity) as daily_total
+                        FROM completed_orders co
+                        JOIN current_day cd on co.day = cd.day
+                        GROUP BY co.day
+                    ),
+                    top_relative_proportions AS (
+                        SELECT co.day, co.potion_sku, ROUND(SUM(co.quantity)::decimal / dt.daily_total, 2) AS relative_potion_probability
+                        FROM completed_orders co
+                        JOIN daily_total dt ON co.day = dt.day
+                        JOIN current_day cd ON co.day = cd.day
+                        GROUP BY co.potion_sku, co.day, dt.daily_total
+                        HAVING ROUND(SUM(co.quantity)::decimal / dt.daily_total, 2) > 0.15
+                        ORDER BY relative_potion_probability DESC
+                        LIMIT 6
+                    ),
+                    random_potions AS (
+                        SELECT potion_sku, potion_quantity
+                        FROM potion_inventory
+                        WHERE potion_quantity > 0
+                        ORDER BY RANDOM()
+                        LIMIT 10
+                    )
+                SELECT p.potion_sku, p.potion_name, p.potion_quantity, p.potion_price, p.potion_type
+                FROM (
+                    SELECT potion_sku, 1 AS priority FROM top_relative_proportions
+                    UNION
+                    SELECT potion_sku, 2 AS priority FROM random_potions
+                ) AS combined_results
+                JOIN potion_inventory p ON combined_results.potion_sku = p.potion_sku
+                WHERE potion_quantity > 0
+                ORDER BY priority, potion_sku
+                LIMIT 6
         '''
         )).fetchall()
 
