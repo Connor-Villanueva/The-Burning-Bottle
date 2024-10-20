d3acf607bc1911cfa7d9a1dfd32e9e5d6c431704
Connor-Villanueva <ckvillan@calpoly.edu>
2024-10-19 18:32:19 -0700

    Bottle Changes

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -56,29 +56,106 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     """ """
     #print(wholesale_catalog)
 
-    purchase_plan = []
-
     with db.engine.begin() as connection:
-        ml_inventory = connection.execute(sqlalchemy.text("SELECT num_red_ml, num_green_ml, num_blue_ml, num_dark_ml FROM global_inventory")).fetchone()
-        ml_max = connection.execute(sqlalchemy.text("SELECT max_ml FROM global_inventory")).fetchone()[0]
-        budget = int(1 * connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory")).fetchone()[0])
+        stats = connection.execute(sqlalchemy.text(
+            """
+                WITH
+                    gold as (
+                        SELECT SUM(gold) as gold
+                        FROM gold_ledger
+                    ),
+                    current_liquid as (
+                        SELECT SUM(red_ml) as red, SUM(green_ml) as green, SUM(blue_ml) as blue, SUM(dark_ml) as dark
+                        FROM barrel_ledger
+                    ),
+                    max_ml as (
+                        SELECT SUM(liquids) as max_ml
+                        FROM capacity_ledger
+                    ),
+                    game_stats as (
+                        SELECT game_stage
+                        FROM game_info
+                    )
 
-    #Sorts barrel catalog by potion_type (r -> g -> b -> d) and by decreasing size
-    barrel_catalog = sorted(filter(lambda b: b.ml_per_barrel > 250, wholesale_catalog), key = lambda b: (b.potion_type, b.ml_per_barrel), reverse=True)
-    print(barrel_catalog)
-    barrel_types = [ [1,0,0,0] , [0,1,0,0] , [0, 0, 1, 0], [0, 0, 0, 1]]
-    ml_needed_each = [ml_max//4 - x for x in ml_inventory]
+                SELECT gold, red, green, blue, dark, max_ml, game_stage
+                FROM gold
+                LEFT JOIN current_liquid ON 1=1
+                LEFT JOIN max_ml ON 1=1
+                LEFT JOIN game_stats ON 1=1
+            """
+        )).fetchone()
 
-    if (budget >= 400):
+        top_daily_potion = connection.execute(sqlalchemy.text(
+            """
+            WITH
+                current_day as (
+                    SELECT day
+                    FROM game_info
+                ),
+                daily_total as (
+                    SELECT co.day, sum(quantity) as total
+                    FROM completed_orders co
+                    JOIN current_day cd on co.day = cd.day
+                    GROUP BY co.day
+                ),
+                relative_probability as (
+                    SELECT p.red, p.green, p.blue, p.dark, ROUND(sum(co.quantity)::numeric / dt.total, 2) as proportion
+                    FROM completed_orders AS co
+                    JOIN daily_total dt on co.day = dt.day
+                    JOIN potions p on p.id = co.potion_id
+                    GROUP BY dt.total, p.red, p.green, p.blue, p.dark
+                )
+            SELECT *
+            FROM relative_probability
+            WHERE red = 100 OR green = 100 OR blue = 100 OR dark = 100
+            ORDER BY proportion DESC
+            LIMIT 1
+            """
+        )).fetchone()
+
+    current_liquids = [stats[1], stats[2], stats[3], stats[4]]
+    top_potion = [top_daily_potion[0], top_daily_potion[1], top_daily_potion[2], top_daily_potion[3]]
+    print(get_barrel_plan(wholesale_catalog, stats[0], current_liquids, stats[5], stats[6], top_potion))
+
+    return []
+
+def get_barrel_plan(catalog: list[Barrel], budget: int, liquids: list[int], max_ml: int, game_state: int, top_potion: list[int]):
+    #   Get barrel plan based on this info
+    purchase_plan = []
+    #   1. Determine catalog with game state
+    if (game_state == 1):
+        catalog = sorted(filter(lambda b: b.ml_per_barrel > 250 and b.ml_per_barrel < 10_000, catalog), key = lambda b: (b.potion_type, b.ml_per_barrel), reverse=True)
+    
+    elif (game_state == 2):
+        catalog = sorted(filter(lambda b: b.ml_per_barrel > 250, catalog), key = lambda b: (b.potion_type, b.ml_per_barrel), reverse=True)
+        budget = 0.85 * budget
+    
+    elif (game_state == 3):
+        catalog = sorted(filter(lambda b: b.ml_per_barrel >= 2500, catalog), key = lambda b: (b.potion_type, b.ml_per_barrel), reverse=True)
+
+    if (budget < 400):
+        barrel_type = [x//100 for x in top_potion]
+        cheapest_barrel = min(filter(lambda b: b.potion_type == barrel_type, catalog), key = lambda b: b.price)
+
+        if (cheapest_barrel.price <= budget):
+            purchase_plan.append(
+                {
+                    "sku": cheapest_barrel.sku,
+                    "quantity": budget // cheapest_barrel.price
+                }
+            )
+    else:
+        ml_needed_each = [max_ml//4 - x for x in liquids]
+        barrel_types = [[1,0,0,0], [0,1,0,0], [0,0,1,0], [0,0,0,1]]
+
         for x in zip(barrel_types, ml_needed_each):
-            max_budget = budget // 4
-            ml = x[1]
-            for barrel in barrel_catalog:
-                if barrel.potion_type == x[0] and ml > 0:
-                    max_qty = ml//barrel.ml_per_barrel
-
+            max_budget = budget//4
+            ml_needed = x[1]
+            for barrel in catalog:
+                if barrel.potion_type == x[0] and ml_needed > 0:
+                    max_qty = ml_needed//barrel.ml_per_barrel
                     max_qty = min(max_qty, max_budget//barrel.price, barrel.quantity)
-                    
+
                     if (max_budget > 0 and max_qty > 0):
                         purchase_plan.append(
                             {
@@ -87,20 +164,6 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
                             }
                         )
                         max_budget -= barrel.price * max_qty
-                        ml -= max_qty * barrel.ml_per_barrel
-    else:
-        min_barrel = min(filter(lambda b: b.ml_per_barrel == 500, barrel_catalog), key = lambda b: b.price)
-        
-        if (min_barrel.price <= budget):
-            purchase_plan.append(
-                {
-                    "sku": min_barrel.sku,
-                    "quantity": budget // min_barrel.price
-                }
-            )
-
+                        ml_needed -= barrel.ml_per_barrel * max_qty
     
-
-
     return purchase_plan
-
--- src/api/bottler.py
+++ src/api/bottler.py
@@ -47,90 +47,147 @@ def get_bottle_plan():
     # Expressed in integers from 1 to 100 that must sum up to 100.
 
     with db.engine.begin() as connection:
+        # potions formatted as [red, green, blue, dark, quantity, proportion]
         potions = connection.execute(sqlalchemy.text(
             """
                 WITH
-                current_day AS (
-                    SELECT latest_day as day
-                    FROM time_info
-                ),
-                daily_total AS (
-                    SELECT co.day, SUM(co.quantity) as daily_total
-                    FROM completed_orders co
-                    GROUP BY co.day
-                ),
-                top_relative_proportions AS (
-                    SELECT co.potion_sku, ROUND(SUM(co.quantity)::decimal / dt.daily_total, 2) AS relative_proportion
-                    FROM completed_orders co
-                    JOIN daily_total dt ON co.day = dt.day
-                    JOIN current_day cd ON co.day = cd.day
-                    GROUP BY co.potion_sku, co.day, dt.daily_total
-                    HAVING ROUND(SUM(co.quantity)::decimal / dt.daily_total, 2) > 0.15
-                    ORDER BY relative_proportion DESC
-                    LIMIT 6
-                ),
-                random_potions AS (
-                    SELECT potion_sku
-                    FROM potion_inventory
-                    ORDER BY random()
-                    LIMIT 10
-                ),
-                total_potions AS (
-                    SELECT potion_sku, SUM(potion_quantity) AS total
-                    FROM potion_inventory
-                    GROUP BY potion_sku
-                ),
-                potion_info AS (
-                    SELECT max_potions, num_red_ml AS red_ml, num_green_ml AS green_ml, num_blue_ml AS blue_ml, num_dark_ml AS dark_ml
-                    FROM global_inventory
-                )
-                SELECT p.potion_type, p.potion_quantity, relative_proportion
-                FROM (
-                SELECT potion_sku, relative_proportion, 1 AS priorty FROM top_relative_proportions
-                UNION
-                SELECT potion_sku, 0, 2 AS priorty FROM random_potions
-                ) AS combined_result
-                JOIN potion_inventory p ON p.potion_sku = combined_result.potion_sku
-                CROSS JOIN potion_info pi
-                ORDER BY priorty ASC
-                LIMIT 6
+                    current_day AS (
+                        SELECT day
+                        FROM game_info
+                    ),
+                    daily_total AS (
+                        SELECT co.day, SUM(co.quantity) as total
+                        FROM completed_orders co
+                        JOIN current_day AS cd ON cd.day = co.day
+                        GROUP BY co.day
+                    ),
+                    top_relative_proportions AS (
+                        SELECT co.potion_id, ROUND(SUM(co.quantity)::decimal / dt.total, 2) as proportion
+                        FROM completed_orders AS co
+                        JOIN daily_total AS dt ON dt.day = co.day
+                        GROUP BY co.potion_id, co.day, dt.total
+                        HAVING ROUND(sum(co.quantity)::decimal / dt.total,2) > 0.15
+                        ORDER BY proportion DESC
+                        LIMIT 6
+                    ),
+                    random_potions AS (
+                        SELECT id as potion_id
+                        FROM potions
+                        WHERE id NOT IN (SELECT potion_id FROM top_relative_proportions)
+                        ORDER BY random()
+                        LIMIT 10
+                    ),
+                    potion_info AS (
+                        SELECT potion_id, p.red, p.green, p.blue, p.dark, proportion
+                        FROM (
+                        SELECT potion_id, 1 AS PRIORITY, proportion FROM top_relative_proportions
+                        UNION
+                        SELECT potion_id, 2 AS PRIORITY, 0 FROM random_potions
+                        ) AS combined_result
+                        JOIN potions p ON p.id = combined_result.potion_id
+                        GROUP BY p.red, p.green, p.blue, p.dark, combined_result.priority, potion_id, combined_result.proportion
+                        ORDER BY priority ASC
+                        LIMIT 6
+                    )
+                SELECT red, green, blue, dark, COALESCE(SUM(quantity),0) as quantity, proportion
+                FROM potion_info
+                LEFT JOIN potion_ledger ON potion_info.potion_id = potion_ledger.potion_id
+                GROUP BY potion_info.potion_id, red, green, blue, dark, proportion
+                ORDER BY proportion DESC, quantity DESC
             """
         )).fetchall()
-
-        inventory_info = connection.execute(sqlalchemy.text(
+        
+        # current inventory stats as [red, green, blue, dark, max_craftable_potions, game_state]
+        stats = connection.execute(sqlalchemy.text(
             """
-                SELECT 
-                max_potions, current_potions, num_red_ml, num_green_ml, num_blue_ml, num_dark_ml
-                FROM global_inventory
-                JOIN (
-                SELECT sum(potion_quantity) as current_potions
-                FROM potion_inventory
-                ) AS total ON 1=1
+            WITH
+                liquids as (
+                    SELECT sum(red_ml) as red, sum(green_ml) as green, sum(blue_ml) as blue, sum(dark_ml) as dark
+                    FROM barrel_ledger
+                ),
+                capacity as (
+                    SELECT sum(potions) as max_potions
+                    FROM capacity_ledger
+                ),
+                current_potions as (
+                    SELECT sum(quantity) as current_potions
+                    FROM potion_ledger
+                    WHERE quantity IS NOT null
+                ),
+                game_state as (
+                select game_stage
+                FROM game_info
+                )
+            SELECT red, green, blue, dark, (max_potions - current_potions) as max_capacity, game_stage
+            FROM liquids
+            JOIN capacity ON 1=1
+            JOIN current_potions on 1=1
+            JOIN game_state on 1=1
             """
         )).fetchone()
-    max_potions = inventory_info[0] - inventory_info[1]
-    liquids = [inventory_info[2], inventory_info[3], inventory_info[4], inventory_info[5]]
     
-    #Each element looks like (potion_type, potion_quantity, relative_probability)
-    for p in potions:
-        ideal_qty = 0
-        max_qty = [b//a for (a,b) in zip(p[0], liquids) if a != 0]
-        if (p[2] > 0):
-            ideal_qty = int(max_potions*p[2])
-        else:
-            ideal_qty = max_potions // len(potions)
+    # Each element looks like (potion_type, potion_quantity, relative_probability)
+    potions = [([p[0], p[1], p[2], p[3]], p[4], p[5]) for p in potions]
+    liquids = [stats[0], stats[1], stats[2], stats[3]]
+
+    potion_plan(potions, liquids, stats[4], stats[5])
+
+    # for p in potions:
+    #     if (p[2] != 0):
+    #         max_qty_each = int(max_craftable_potions * p[2])
+    #     else:
+    #         proportion = ((1-sum(x[2] for x in potions))/6)
+    #         max_qty_each = int(max_craftable_potions * proportion)
+    #     max_qty = [a//b for (a,b) in zip(liquids, p[0]) if b != 0]
+    #     max_qty = min(max_qty_each, min(max_qty))
+
+    #     print(max_qty)
         
-        max_qty = min(ideal_qty, min(max_qty))
-        if (max_qty > 0):
-            bottle_plan.append(
-                {
-                    "potion_type": p[0],
-                    "quantity": max_qty
-                }
-            )
-        liquids = [b - a*max_qty for (a,b) in zip(p[0],liquids)]
- 
-    return bottle_plan
+    return []
+
+"""
+Note:   potions is sorted in desc order of potion proportions
+        Thus allowing for higher proportions to have priority
+"""
+def potion_plan(potions: list[int, float], liquids: list[int], max_potions: int, game_state: int):
+    potion_plan = []
+
+    
+    if (game_state == 1):
+        qty_each = max_potions//6
+
+        for p in potions:
+            qty_craftable = min([a//b for (a,b) in zip(liquids, p[0]) if b != 0])
+            qty_craftable = min(qty_each, qty_craftable) - p[1]
+
+            if (qty_craftable > 0):
+                potion_plan.append(
+                    {
+                        "potion_type": p[0],
+                        "quantity": qty_craftable
+                    }
+                )
+                liquids = [a-b*qty_craftable for (a,b) in zip(liquids, p[0])]
+
+    elif (game_state == 2):
+        for p in potions:
+            qty_craftable = min([a//b for (a,b) in zip(liquids, p[0]) if b != 0])
+            if p[2] > 0:
+                qty_craftable = min(int(max_potions*p[2]), qty_craftable) - p[1]
+
+                if (qty_craftable > 0):
+                    potion_plan.append(
+                        {
+                            "potion_type": p[0],
+                            "quantity": qty_craftable
+                        }
+                    )
+
+                
+            
+    print(potion_plan)
+
+    return potion_plan
 
 if __name__ == "__main__":
     print(get_bottle_plan())
